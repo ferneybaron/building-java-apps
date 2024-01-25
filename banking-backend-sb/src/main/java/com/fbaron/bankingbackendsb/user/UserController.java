60abdae962f0561fa89aada3b99b04a737ce6b27
SujataE <sujata.edekar@gmail.com>
2024-01-24 17:14:33 -0700

    1124 - 01  Updated Controller to maintain SOLID first principle single responsibility

--- banking-backend-sb/src/main/java/com/fbaron/bankingbackendsb/user/UserController.java
+++ banking-backend-sb/src/main/java/com/fbaron/bankingbackendsb/user/UserController.java
@@ -6,22 +6,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
+// To maintain the SOLID principle we have only one responsibility
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
-    Map<String, User> users = new HashMap<>();
+//    Map<String, User> users = new HashMap<>();
+
+    UserService userService = new UserService();
 
     // Create user
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        if (Objects.isNull(user.username) || Objects.isNull(user.password) || user.username.isBlank() || user.password.isBlank())
-            return ResponseEntity.badRequest().build();
-        users.put(user.username, user);
-        return ResponseEntity.created(null).body(user);
+//        if (Objects.isNull(user.username) || Objects.isNull(user.password) || user.username.isBlank() || user.password.isBlank())
+//            return ResponseEntity.badRequest().build();
+//        users.put(user.username, user);
+        User createUser = userService.createUser(user);
+//        return ResponseEntity.created(null).body(user);
+        return ResponseEntity.created(null).body(createUser);
     }
 }
 
